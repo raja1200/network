5ab0932c4176815019b4e4ab27bfe4a3e4d28e51
Esakki Pandi <esakkipandi12015@gmail.com>
2025-05-27 22:46:34 +0530

    Add files via upload

---
+++ (8)dataEncryptionDryption.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include<string.h>
+
+
+int main(){
+	int i , length , choice;
+	char str[100];
+	
+	printf("Enter the message : ");
+	gets(str); //one line input for array(string) without using loop
+	
+	length = strlen(str);
+	
+	printf("Enter your option : ");
+	printf("\n1.Encryption \n2.Decryption");
+	scanf("%d",&choice);
+	
+	switch(choice){
+		case 1:
+			for(i= 0 ; i<length ; i++)
+			{
+				str[i] = str[i] + 3 ;
+			}
+			printf("The Encrypted string : %s", str);
+			break ;
+	
+	    case 2:
+			for(i= 0 ; i<length ; i++)
+			{
+				str[i] = str[i] - 3 ;
+			}
+			printf("The Decypted string : %s", str);
+	    	break;
+  	    default :
+  	    	printf("Invalid Choice");
+}
+return 0;
+}
